6a4fb6333ca192d026c894ba656e276d4ae95548
Renaud MEYNADIER <rmeynadier@ALMERYS.local>
2021-02-26 16:47:20 +0100

    Ajout du serveur mock et des services

---
+++ The-Tour/src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: [ './hero-detail.component.css' ]
+})
+export class HeroDetailComponent implements OnInit {
+  hero: (Hero | undefined);
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = +(this.route.snapshot.paramMap.get('id') ?? 0);
+    console.log(id);
+    this.heroService.getHero(id)
+      .subscribe(hero => {
+        this.hero = hero;
+        this.heroService.getHeroes().subscribe(heroes => console.log(heroes));
+        console.log(hero);
+
+      });
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
+}
